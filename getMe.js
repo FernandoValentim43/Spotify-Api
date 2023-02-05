5603f998f565665391cdda2208fb9228ac48c00d
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-02-05 20:51:09 -0300

    get saved songs

--- getMe.js
+++ getMe.js
@@ -1,17 +1,33 @@
-const fs = require("fs")
-const SpotifyWebApi = require("spotify-web-api-node")
-const TOKEN = process.env.TOKEN;
+const fs = require("fs");
+require("dotenv").config();
+
+const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken(TOKEN)
+spotifyApi.setAccessToken(process.env.TOKEN);
 
 function getMyData() {
-    (async () => {
-        const me = await spotifyApi.getMe();
-       /*  console.log(me.body); */
-        getUserPlaylists(me.body.id);
-    })().catch(e => {
-        console.error(e)
-    })
+  (async () => {
+    const me = await spotifyApi.getMe();
+    /*  console.log(me.body); */
+    getUserPlaylists(me.body.id);
+  })().catch((e) => {
+    console.error(e);
+  });
 }
 
-getMyData()
+async function getUserPlaylists(userName) {
+  const data = await spotifyApi.getMySavedTracks(userName);
+
+  console.log("-------------------------------------------------------");
+
+  /* for (let i = 0; i < data.body.items.length; i++) {
+    console.log(
+      "SONG NAME: " +
+        data.body.items[i].track.name +
+        " ----- ARTIST: " +
+        data.body.items[i].track.artists[0].name
+    );
+  } */
+}
+
+getMyData();
